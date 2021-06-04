867a2a75de449337227d23b88e08ed00ea0f4572
shirzadimasoud <shirzadimasoud@gmail.com>
2021-06-05 01:35:40 +0430

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,267 @@
+﻿
+namespace Methods
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.getNameBTN = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.num1_txtBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.sumBTN = new System.Windows.Forms.Button();
+            this.num2_txtbox = new System.Windows.Forms.TextBox();
+            this.search_txtbox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.searchBTN = new System.Windows.Forms.Button();
+            this.list_button = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.list_textbox = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.removeList_btn = new System.Windows.Forms.Button();
+            this.listUpdate_txtbox = new System.Windows.Forms.TextBox();
+            this.updateList_btn = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // getNameBTN
+            // 
+            this.getNameBTN.Location = new System.Drawing.Point(272, 35);
+            this.getNameBTN.Name = "getNameBTN";
+            this.getNameBTN.Size = new System.Drawing.Size(101, 41);
+            this.getNameBTN.TabIndex = 0;
+            this.getNameBTN.Text = "Get Name";
+            this.getNameBTN.UseVisualStyleBackColor = true;
+            this.getNameBTN.Click += new System.EventHandler(this.getNameBTN_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Name:";
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Location = new System.Drawing.Point(119, 42);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(138, 22);
+            this.nameTextBox.TabIndex = 2;
+            // 
+            // num1_txtBox
+            // 
+            this.num1_txtBox.Location = new System.Drawing.Point(119, 139);
+            this.num1_txtBox.Name = "num1_txtBox";
+            this.num1_txtBox.Size = new System.Drawing.Size(138, 22);
+            this.num1_txtBox.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 161);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(107, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Enter Numbers:";
+            // 
+            // sumBTN
+            // 
+            this.sumBTN.Location = new System.Drawing.Point(272, 139);
+            this.sumBTN.Name = "sumBTN";
+            this.sumBTN.Size = new System.Drawing.Size(101, 62);
+            this.sumBTN.TabIndex = 3;
+            this.sumBTN.Text = "Sum";
+            this.sumBTN.UseVisualStyleBackColor = true;
+            this.sumBTN.Click += new System.EventHandler(this.sumBTN_Click);
+            // 
+            // num2_txtbox
+            // 
+            this.num2_txtbox.Location = new System.Drawing.Point(119, 179);
+            this.num2_txtbox.Name = "num2_txtbox";
+            this.num2_txtbox.Size = new System.Drawing.Size(138, 22);
+            this.num2_txtbox.TabIndex = 6;
+            // 
+            // search_txtbox
+            // 
+            this.search_txtbox.Location = new System.Drawing.Point(119, 248);
+            this.search_txtbox.Name = "search_txtbox";
+            this.search_txtbox.Size = new System.Drawing.Size(138, 22);
+            this.search_txtbox.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 253);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 17);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Search:";
+            // 
+            // searchBTN
+            // 
+            this.searchBTN.Location = new System.Drawing.Point(272, 241);
+            this.searchBTN.Name = "searchBTN";
+            this.searchBTN.Size = new System.Drawing.Size(101, 41);
+            this.searchBTN.TabIndex = 7;
+            this.searchBTN.Text = "Find";
+            this.searchBTN.UseVisualStyleBackColor = true;
+            this.searchBTN.Click += new System.EventHandler(this.searchBTN_Click);
+            // 
+            // list_button
+            // 
+            this.list_button.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.list_button.Location = new System.Drawing.Point(59, 123);
+            this.list_button.Name = "list_button";
+            this.list_button.Size = new System.Drawing.Size(57, 39);
+            this.list_button.TabIndex = 10;
+            this.list_button.Text = "+";
+            this.list_button.UseVisualStyleBackColor = true;
+            this.list_button.Click += new System.EventHandler(this.list_button_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(73, 57);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(94, 17);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Enter Names:";
+            // 
+            // list_textbox
+            // 
+            this.list_textbox.Location = new System.Drawing.Point(15, 86);
+            this.list_textbox.Name = "list_textbox";
+            this.list_textbox.Size = new System.Drawing.Size(221, 22);
+            this.list_textbox.TabIndex = 12;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.updateList_btn);
+            this.groupBox1.Controls.Add(this.listUpdate_txtbox);
+            this.groupBox1.Controls.Add(this.removeList_btn);
+            this.groupBox1.Controls.Add(this.listBox1);
+            this.groupBox1.Controls.Add(this.list_textbox);
+            this.groupBox1.Controls.Add(this.list_button);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Location = new System.Drawing.Point(417, 16);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(554, 305);
+            this.groupBox1.TabIndex = 13;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "List of members";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(253, 19);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(280, 276);
+            this.listBox1.TabIndex = 13;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // removeList_btn
+            // 
+            this.removeList_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.removeList_btn.Location = new System.Drawing.Point(132, 123);
+            this.removeList_btn.Name = "removeList_btn";
+            this.removeList_btn.Size = new System.Drawing.Size(57, 39);
+            this.removeList_btn.TabIndex = 14;
+            this.removeList_btn.Text = "-";
+            this.removeList_btn.UseVisualStyleBackColor = true;
+            this.removeList_btn.Click += new System.EventHandler(this.removeList_btn_Click);
+            // 
+            // listUpdate_txtbox
+            // 
+            this.listUpdate_txtbox.Location = new System.Drawing.Point(15, 196);
+            this.listUpdate_txtbox.Name = "listUpdate_txtbox";
+            this.listUpdate_txtbox.Size = new System.Drawing.Size(221, 22);
+            this.listUpdate_txtbox.TabIndex = 15;
+            // 
+            // updateList_btn
+            // 
+            this.updateList_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.updateList_btn.Location = new System.Drawing.Point(59, 232);
+            this.updateList_btn.Name = "updateList_btn";
+            this.updateList_btn.Size = new System.Drawing.Size(126, 39);
+            this.updateList_btn.TabIndex = 16;
+            this.updateList_btn.Text = "Update List";
+            this.updateList_btn.UseVisualStyleBackColor = true;
+            this.updateList_btn.Click += new System.EventHandler(this.updateList_btn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(983, 333);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.search_txtbox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.searchBTN);
+            this.Controls.Add(this.num2_txtbox);
+            this.Controls.Add(this.num1_txtBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.sumBTN);
+            this.Controls.Add(this.nameTextBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.getNameBTN);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button getNameBTN;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.TextBox num1_txtBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button sumBTN;
+        private System.Windows.Forms.TextBox num2_txtbox;
+        private System.Windows.Forms.TextBox search_txtbox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button searchBTN;
+        private System.Windows.Forms.Button list_button;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox list_textbox;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button removeList_btn;
+        private System.Windows.Forms.Button updateList_btn;
+        private System.Windows.Forms.TextBox listUpdate_txtbox;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Methods
+{
+    public partial class Form1 : Form
+    {
+        List<string> names = new List<string>();
+        int _index;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void getNameBTN_Click(object sender, EventArgs e)
+        {
+            getname_function(nameTextBox.Text);
+
+        }
+
+        private void getname_function(string name)
+        {
+            MessageBox.Show($"Your name is : {name}", "Get Name", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void sumBTN_Click(object sender, EventArgs e)
+        {
+            int num1 = int.Parse(num1_txtBox.Text);
+            int num2 = int.Parse(num2_txtbox.Text);
+
+            int sum = _sum(num1, num2);
+            MessageBox.Show($"Sum is = {sum}", "Sum", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private int _sum(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        private void searchBTN_Click(object sender, EventArgs e)
+        {
+            string[] names = { "ali", "hasan", "hossein", "masoud" };
+
+            string name_to_find = search_txtbox.Text;
+
+            bool find = _search(names, name_to_find);
+            if (find)
+            {
+                MessageBox.Show($"We found the name: {name_to_find}", "Search", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show($"We cannot find the name: {name_to_find}", "Search", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            
+        }
+
+        private bool _search(string[] names, string name_to_find)
+        {
+            bool find = false;
+            for (int i = 0; i < names.Length; i++)
+            {
+                if (names[i] == name_to_find) 
+                {
+                    
+                    find = true;
+
+                }
+            }
+            return find;
+        }
+
+        private void list_button_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add(list_textbox.Text);
+        }
+
+        private void removeList_btn_Click(object sender, EventArgs e)
+        {
+            int index = listBox1.SelectedIndex;
+            listBox1.Items.RemoveAt(index);
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+            listUpdate_txtbox.Text = (string) listBox1.SelectedItem;
+            _index = listBox1.SelectedIndex;
+            
+        }
+
+        private void updateList_btn_Click(object sender, EventArgs e)
+        {
+            names = listBox1.Items.Cast<string>().ToList();
+            names[_index] = listUpdate_txtbox.Text;
+            listBox1.Items.Clear();
+            for (int i = 0; i < names.Count; i++)
+            {
+                listBox1.Items.Add(names[i]);
+            }
+            
+        }
+    }
+}
